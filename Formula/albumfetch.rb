8b9d9f9c023ec063343193746c71b50c5acee645
StuffedRat46810 <alon@moranoa.com>
2026-01-27 10:05:33 +0200

    changed sha256

--- Formula/albumfetch.rb
+++ Formula/albumfetch.rb
@@ -2,7 +2,7 @@ class Albumfetch < Formula
   desc "a CLI tool that displays random albums"
   homepage "https://github.com/StuffedRat46810/albumfetch"
   url "https://github.com/StuffedRat46810/albumfetch/releases/download/v0.1.2/albumfetch-macos.tar.gz"
-  sha256 "43f5d7e56909b4f9e2d66ba7a0c9cf869bbcce288365b2b2848cd3040d462f32"
+  sha256 "e3ef1334ccd756ac919554d3917fb86cb98977a58dc85a0699f0e574c06b64da"
   license "MIT"
 
 
