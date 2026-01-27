932b50e79245ae5be77253d394c6ba89dfb76072
StuffedRat46810 <alon@moranoa.com>
2026-01-27 10:06:38 +0200

    changed bin path

--- Formula/albumfetch.rb
+++ Formula/albumfetch.rb
@@ -1,7 +1,7 @@
 class Albumfetch < Formula
   desc "a CLI tool that displays random albums"
   homepage "https://github.com/StuffedRat46810/albumfetch"
-  url "https://github.com/StuffedRat46810/albumfetch/releases/download/v0.1.2/albumfetch-macos.tar.gz"
+  url "https://github.com/StuffedRat46810/albumfetch/releases/download/v0.1.2/albumfetch-v0.1.2-macos.tar.gz"
   sha256 "e3ef1334ccd756ac919554d3917fb86cb98977a58dc85a0699f0e574c06b64da"
   license "MIT"
 
